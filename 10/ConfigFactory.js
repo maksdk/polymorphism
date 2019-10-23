9911988890db21d302b1859f4ce1db9f308548ab
maksdk <havryshchenko@gmail.com>
2019-10-23 08:08:05 +0300

    initial work with factory

---
+++ 10/ConfigFactory.js
@@ -0,0 +1,9 @@
+import path from 'path';
+import fs from 'fs';
+import JsonParser from './parsers/JsonParser';
+import YamlParser from './parsers/YamlParser';
+import Config from './Config';
+
+// BEGIN (write your solution here)
+
+// END
